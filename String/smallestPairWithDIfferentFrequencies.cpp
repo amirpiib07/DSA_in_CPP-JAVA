730a06a33dc4173e1a867d26be6da10a67c1781f
AMIT KUMAR SINGH <amirpiib07@gmail.com>
2026-02-28 22:03:06 +0530

    Implement function to find min distinct frequency pair

---
+++ String/smallestPairWithDIfferentFrequencies.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> minDistinctFreqPair(vector<int>& nums) {
+        unordered_map<int,int> f;
+        for(int x: nums){
+            f[x]++;
+        }
+        vector<int>v;
+        for(auto &p: f){
+            v.push_back(p.first);
+        }
+        sort(v.begin(),v.end());
+        
+        vector<int> ans={-1,-1};
+        for(int i=0;i<v.size();i++){
+            for(int j=i+1;j<v.size();j++){
+                if(f[v[i]]!=f[v[j]]) return {v[i],v[j]};
+            }
+        }
+
+        return ans;
+    
+    }
+};
