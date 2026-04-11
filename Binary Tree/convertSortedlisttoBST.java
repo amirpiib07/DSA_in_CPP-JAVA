f8b9bbe14aba4544ae350fc846e20ac59fb485b5
AMIT KUMAR SINGH <amirpiib07@gmail.com>
2026-04-12 00:03:58 +0530

    Create convertSortedlisttoBST.java

---
+++ Binary Tree/convertSortedlisttoBST.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) return null;
+        return build(head);
+    }
+
+    public TreeNode build(ListNode head) {
+        if (head == null) return null;
+        if (head.next == null) return new TreeNode(head.val);
+
+        ListNode slow = head, fast = head, prev = null;
+
+        // Find middle
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Break left half
+        if (prev != null) prev.next = null;
+
+        TreeNode root = new TreeNode(slow.val);
+
+        // Left subtree
+        if (slow != head) 
+            root.left = build(head);
+
+        // Right subtree
+        root.right = build(slow.next);
+
+        return root;
+    }
+}
