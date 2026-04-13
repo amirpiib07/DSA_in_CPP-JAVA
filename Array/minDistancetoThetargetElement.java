7a38e7e8ef19c968423088faac7d5cecc2784f72
AMIT KUMAR SINGH <amirpiib07@gmail.com>
2026-04-13 19:25:15 +0530

    Update minDistancetoThetargetElement.java

--- Array/minDistancetoThetargetElement.java
+++ Array/minDistancetoThetargetElement.java
@@ -10,3 +10,25 @@ class Solution {
         return min;
     }
 }
+
+//Optimal Solution 
+
+class Solution {
+    public int getMinDistance(int[] nums, int target, int start) {
+        int n = nums.length;
+        
+        for (int d = 0; d < n; d++) {
+            // check left
+            if (start - d >= 0 && nums[start - d] == target) {
+                return d;
+            }
+            
+            // check right
+            if (start + d < n && nums[start + d] == target) {
+                return d;
+            }
+        }
+        
+        return -1; // just in case (though problem guarantees existence)
+    }
+}
