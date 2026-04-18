4455cafdbb46c4d69d696beab7c939c2d5a29e79
AMIT KUMAR SINGH <amirpiib07@gmail.com>
2026-04-18 23:46:30 +0530

    Implement mirror of integer and distance calculation

---
+++ Math/mirrorofinteger.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int f(int n) {
+        int ans = 0;
+        while (n != 0) {
+            ans = ans * 10 + (n % 10);
+            n /= 10;
+        }
+        return ans;
+    }
+    int mirrorDistance(int n) { return abs(n - f(n)); }
+};
