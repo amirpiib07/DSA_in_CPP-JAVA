4a0e8a57ddaea17fdbfaee832dddde018ead5934
AMIT KUMAR SINGH <amirpiib07@gmail.com>
2026-06-06 16:27:10 +0530

    Create mininrotatedsortedarray.java

---
+++ Binary_Search/mininrotatedsortedarray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        int min = nums[0];
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            min = Math.min(min, nums[mid]);
+
+            if (nums[mid] > nums[r]) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return min;
+    }
+}
